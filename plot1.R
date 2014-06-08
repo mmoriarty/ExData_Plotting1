904e5640b23c09691121fc018e27a88e83ee27b9
mmoriarty <moriarty.michelle@gmail.com>
2014-06-08 18:15:46 -0500

    Revise plot 1
    Revise plot 1

--- plot1.R
+++ plot1.R
@@ -1,9 +1,8 @@
-data<-read.table("//amwins/chi$/chiuser$/moriamic/Desktop/Coursera/Exploratory Data Analysis/HW1/household_power_consumption.txt",header=TRUE,sep = ";")
+data<-read.table("//amwins/chi$/chiuser$/moriamic/Desktop/Coursera/Exploratory Data Analysis/HW1/household_power_consumption.txt",header=TRUE,sep = ";",colClasses = c('character', 'character','numeric', 'numeric', 'numeric', 'numeric', 'numeric','numeric', 'numeric'), na.strings='?')
 
-sub <- subset(data, as.Date(Date) >="02/01/2007" & as.Date(Date) <="02/02/2007" & !is.na(Global_active_power))
+data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+sub <- subset(data, as.Date(DateTime) >= as.Date("2007-02-01") & as.Date(DateTime) <= as.Date("2007-02-02"))
 
-gph<-as.numeric(as.character(sub$Global_active_power))
-
-jpeg('plot1.jpg')
-hist(gph,main="Global Active Power",xlab="Global Active Power (kilowatts)",col='red')
+png('plot1.png')
+hist(sub$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col='red')
 dev.off()
